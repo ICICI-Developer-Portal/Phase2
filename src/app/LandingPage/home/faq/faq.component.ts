cfb57fac6e416e37e0cd9c4099385f71f2257544
Raksha <raksha.bhat@niveussolutions.com>
2020-05-12 20:04:50 +0530

    faq header added

--- src/app/LandingPage/home/faq/faq.component.ts
+++ src/app/LandingPage/home/faq/faq.component.ts
@@ -13,6 +13,7 @@ export class FaqComponent implements OnInit {
   faqObjQues:any=[];
   faqObjAns:any=[];
   faqObjList:any;
+  faqHeaderList:any;
   constructor(
     private adm: LoginService,
   ) { }
@@ -29,6 +30,8 @@ export class FaqComponent implements OnInit {
           return str.replace(/\\n/g, '\n')
         });
         }
+      this.faqHeaderList = JSON.parse(this.faqObjList)
+      this.faqHeader = this.faqHeaderList["1"][0]
     })
   }
 }
